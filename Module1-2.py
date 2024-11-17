3a7487b914bcf566cb0034c6d39759cb1df7fb66
oabdaljalil <60644333+oabdaljalil@users.noreply.github.com>
2024-11-17 12:17:12 -0700

    Add files via upload

---
+++ Module1-2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+num1 = input('input first number: ')
+num2 = input('input second number: ')
+mresult = int(num1) * int(num2)
+dresult = int(num1) / int(num2)
+print('the multiplication of both numbers is: ', mresult)
+print('the division of both numbers (first / second) is: ', dresult)
