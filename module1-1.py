0cca32c54358ea45fb67313a59d5c65f1f9a5038
oabdaljalil <60644333+oabdaljalil@users.noreply.github.com>
2024-11-17 11:45:36 -0700

    Update module1-1.py

--- module1-1.py
+++ module1-1.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
 
+num1 = input('input first number: ')
+num2 = input('input second number: ')
+addresult = int(num1) + int(num2)
+subtresult = int(num1) - int(num2)
+print('the addition of both numbers is: ', addresult)
+print('the subtraction of both numbers (first - second) is: ', subtresult)
